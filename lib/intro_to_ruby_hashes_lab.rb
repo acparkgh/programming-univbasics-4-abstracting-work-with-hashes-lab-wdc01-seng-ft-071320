a5b7f0941e17da0333ead48a69d74a937c32c863
Andrew Park <acpark123@gmail.com>
2020-06-07 21:37:45 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -17,11 +17,8 @@ def update_counting_hash(hash, key)
   
   #   hash[key] == nil ? hash[key] = 1 : hash[key] += 1
   
-    if hash[key] == nil
-       hash[key] = 1
-    else
-       hash[key] += 1
-    end
+    hash.default(0)
+    hash[key] += 1
     
     hash
 end
